606ae1df5967b998612227c6563a2813306a58a1
Quy Nguyen <youngboy5801@gmail.com>
2020-07-12 16:25:00 +0700

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/86636749

---
+++ codeforces/1372/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<array<int,2>> vii;
+typedef vector<int> vi;
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    int n;
+    cin >> n;
+    while (n--) {
+        cout << 1 << " ";
+    } 
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
