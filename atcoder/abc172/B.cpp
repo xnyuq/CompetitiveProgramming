620cbb75191e63c27ab7c2b0e7ba5c76b966f0df
Quy Nguyen <youngboy5801@gmail.com>
2020-07-22 17:44:00 +0700

    Add solution for problem `B - Minor Change` Link: https://atcoder.jp/contests/abc172/tasks/abc172_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc172/submissions/15360717

---
+++ atcoder/abc172/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    string s,t;
+    cin >> s >> t;
+    int ans = 0;
+    rep(i,0,sz(s)) {
+        ans += s[i] != t[i];
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
