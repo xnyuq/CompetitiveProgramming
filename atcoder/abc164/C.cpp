2290f69ce654b4b8964ee9af600dcc19fe6d17b8
Quy Nguyen <youngboy5801@gmail.com>
2020-07-22 13:05:00 +0700

    Add solution for problem `C - gacha` Link: https://atcoder.jp/contests/abc164/tasks/abc164_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc164/submissions/15357335

---
+++ atcoder/abc164/C.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    unordered_set<string> s;
+    int n;
+    cin >> n;
+    rep(i,0,n) {
+        string tmp;
+        cin >> tmp;
+        s.emplace(tmp);
+    }
+    cout << sz(s); 
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
