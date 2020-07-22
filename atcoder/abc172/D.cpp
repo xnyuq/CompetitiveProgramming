469c30ea52aef6bfaf44164830a1c9b92e4debf9
Quy Nguyen <youngboy5801@gmail.com>
2020-07-22 21:56:00 +0700

    Add solution for problem `D - Sum of Divisors` Link: https://atcoder.jp/contests/abc172/tasks/abc172_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc172/submissions/15364678

---
+++ atcoder/abc172/D.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    int n;
+    cin >> n;
+    ll ans = 0;
+    rep(i, 1, n+1) {
+        ll x = n / i;
+        ans += i * (x * (x + 1) / 2);
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
