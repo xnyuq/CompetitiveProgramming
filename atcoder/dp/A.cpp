b794599bb544c7eb1468527077c7924ba38eb802
Quy Nguyen <youngboy5801@gmail.com>
2020-07-23 13:48:00 +0700

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/15372619

---
+++ atcoder/dp/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    int n;
+    cin >> n;
+    vi dp(n, INF), a(n);
+    for (auto &i: a) {
+        cin >> i;
+    }
+    dp[0] = 0;
+    rep(i,0,n) {
+        dp[i + 1] = min(dp[i+1], dp[i] + abs(a[i] - a[i+1]));
+        dp[i + 2] = min(dp[i+2], dp[i] + abs(a[i] - a[i+2]));
+    }
+    cout << dp[n-1] << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
