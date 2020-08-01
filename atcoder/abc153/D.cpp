3fb228629d8581612db0c75f74ca27f5f15b5254
Quy Nguyen <youngboy5801@gmail.com>
2020-08-01 20:09:00 +0700

    Add solution for problem `D - Caracal vs Monster` Link: https://atcoder.jp/contests/abc153/tasks/abc153_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc153/submissions/15569256

---
+++ atcoder/abc153/D.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+ll ans;
+
+void solve() {
+    ll n, i = 1;
+    cin >> n;
+    while (n ^ 0) {
+        n >>= 1;
+        ans += i;
+        i <<= 1;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
