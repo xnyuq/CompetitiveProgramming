c0ad51ee3f1e12ddbce171ed7a59ebbf2ad030a7
Quy Nguyen <youngboy5801@gmail.com>
2020-08-04 18:21:00 +0700

    Add solution for problem `B - Distance` Link: https://atcoder.jp/contests/abc174/tasks/abc174_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc174/submissions/15686542

---
+++ atcoder/abc174/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    ll ans = 0, n,a,b,d;
+    cin >> n >> d;
+    rep(i,0,n) {
+        cin >> a >> b;
+        if (a*a+b*b <= d*d) ++ans;
+    }
+    cout << ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
