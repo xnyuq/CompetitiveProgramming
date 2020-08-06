581e17ba7348bbec8c5b0a43ed5edbd48d815616
Quy Nguyen <youngboy5801@gmail.com>
2020-08-06 17:51:00 +0700

    Add solution for problem `B - Trained?` Link: https://atcoder.jp/contests/abc065/tasks/abc065_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc065/submissions/15722948

---
+++ atcoder/abc065/B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define INF 1000000000
+#define MOD 1000000007
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n+1);
+    rep(i,1,n+1) {
+        cin >> a[i];
+    }
+    int ans = 1, i = 1;
+    set<int> s{1};
+    while (a[i] != 2) {
+        i = a[i];
+        if (i != 2 && s.find(i) != s.end()) {
+            cout << -1;
+            return;
+        }
+        s.emplace(i);
+        ++ans;
+    }
+    cout << ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
