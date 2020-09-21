baa9468e86115a81c71d16ab0f80ed29091389b9
Quy Nguyen <youngboy5801@gmail.com>
2020-09-21 17:32:00 +0700

    Add solution for problem `C - Mortal Kombat Tower` Link: https://codeforces.com/contest/1418/problem/C Tags: dp, graphs, greedy, shortest paths, *1500 Ref: https://codeforces.com/contest/1418/submission/93396012

--- codeforces/1418/C.cpp
+++ codeforces/1418/C.cpp
@@ -6,15 +6,30 @@ void solve() {
     int n;
     cin >> n;
     vector<int> a(n);
-    vector<int> him(n, INF), us(n, INF);
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, INF)));
     for (auto &i: a) cin >> i;
-    us[1] = him[0] = a[0];
-    him[1] = him[0] + a[1];
-    for (int i = 2; i < n; ++i) {
-        him[i] = min(us[i-1]+a[i], us[i-2] + a[i-1] + a[i]);
-        us[i] = min(him[i-2], him[i-1]);
+    dp[0][0][1] = (a[0] == 1); 
+    for (int i = 1; i < n; ++i) {
+        for (int p = 0; p < 2; ++p) {
+            dp[i][p][0] = dp[i-1][p][1] + (p == 0 && a[i] == 1);
+            dp[i][p][1] = min(dp[i-1][p^1][1], dp[i-1][p^1][0]) + (p == 0 && a[i] == 1);
+        }
     }
-    cout << min(him[n-1], us[n-1]) << "\n";
+    int ans = INF;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            ans  = min(ans, dp[n-1][i][j]);
+        }
+    }
+    cout << ans << "\n";
+    //for (int i = 0; i < n; ++i) {
+        //for (int j = 0; j < 2; ++j) {
+            //for (int k = 0; k < 2; ++k){
+                //printf("dp[%d][%d][%d] = %d\n",i,j,k,dp[i][j][k]);
+            //}
+        //}
+    //}
+    //cout << "----------------------------\n";
 }
 
 int main() {
