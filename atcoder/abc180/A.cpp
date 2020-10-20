0fd3095bfb36f16fda4d41374ecaf3bae6f88362
Quy Nguyen <youngboy5801@gmail.com>
2020-10-20 23:31:00 +0700

    Add solution for problem `A - box` Link: https://atcoder.jp/contests/abc180/tasks/abc180_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc180/submissions/17547981

---
+++ atcoder/abc180/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n,a,b;
+    cin >> n >> a >> b;
+    cout << n -a +b << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
+
