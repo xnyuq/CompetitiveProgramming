be9a02854458923abf041470e53729edfbf7f911
Quy Nguyen <youngboy5801@gmail.com>
2020-11-23 02:09:00 +0700

    Add solution for problem `D - Radio Towers` Link: https://codeforces.com/contest/1452/problem/D Tags: combinatorics, dp, math, *1600 Ref: https://codeforces.com/contest/1452/submission/99275947

--- codeforces/1452/D.cpp
+++ codeforces/1452/D.cpp
@@ -3,21 +3,16 @@
 using namespace std;
 
 const int MOD = 998244353;
-int binpow(int a, int b) {
-    a %= MOD;
-    b %= MOD - 1;
-    int64_t res = 1;
-    while (b > 0) {
-        if (b & 1) res = res * a % MOD;
-        a = 1ll * a * a % MOD;
-        b >>= 1;
-    }
-    return res;
+int64_t binpow(int a, int b) {
+    if (b == 0) return 1;
+    int64_t res = binpow(a,b/2);
+    if (b&1) return res*res%MOD*(a%MOD)%MOD;
+    else return res*res%MOD;
 }
 
 int divMod(int a,int b) {
     a %= MOD;
-    return a * 1ll * binpow(b, MOD-2) % MOD;
+    return a * binpow(b, MOD-2) % MOD;
 }
 
 void solve() {
