0ee53b23d9a144056c3bcac391755c04cf119102
Quy Nguyen <youngboy5801@gmail.com>
2020-11-25 21:00:00 +0700

    Add solution for problem `C - The Delivery Dilemma` Link: https://codeforces.com/contest/1443/problem/C Tags: binary search, greedy, sortings, *1400 Ref: https://codeforces.com/contest/1443/submission/99566377

--- codeforces/1443/C.cpp
+++ codeforces/1443/C.cpp
@@ -1,30 +1,30 @@
-    // time-limit: 2000
-    #include <bits/stdc++.h>
-    using namespace std;
-     
-    void solve() {
-        long long n, passedTime = 0;
-        cin >> n;
-        vector<pair<int, int>> arr(n);
-        for (auto &i : arr)
-            cin >> i.first;
-        for (auto &i : arr)
-            cin >> i.second;
-        sort(arr.rbegin(), arr.rend());
-        int i =0;
-        for (; i < n && passedTime + arr[i].second < arr[i].first; ++i)
-            passedTime += arr[i].second;
-        long long ans = passedTime;
-        if (i < n) ans = max(ans, 1ll*arr[i].first);
-        cout << ans << "\n";
+// time-limit: 2000
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n, passedTime = 0;
+    cin >> n;
+    vector<array<int, 2>> arr(n);
+    for (auto &i : arr)
+        cin >> i[0];
+    for (auto &i : arr)
+        cin >> i[1];
+    sort(arr.rbegin(), arr.rend());
+    int i =0;
+    for (; i < n && passedTime + arr[i][1] < arr[i][0]; ++i)
+        passedTime += arr[i][1];
+    long long ans = passedTime;
+    if (i < n) ans = max(ans, 1ll*arr[i][0]);
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
     }
-     
-    int main() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int t = 1;
-        cin >> t;
-        while (t--) {
-            solve();
-        }
-    }
+}
