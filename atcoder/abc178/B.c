ab258124fb386b195234629cb65c87fe3262add4
Quy Nguyen <youngboy5801@gmail.com>
2020-12-04 22:10:00 +0700

    Add solution for problem `B - Product Max` Link: https://atcoder.jp/contests/abc178/tasks/abc178_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc178/submissions/18562066

--- atcoder/abc178/B.c
+++ atcoder/abc178/B.c
@@ -1,16 +1,12 @@
-        #include<stdio.h>
-        long long ab(long long a) {
-        	return (a>=0?a:-a);
-        }
-        int main(){
-        	long long a,b,c,d,max;
-        	scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
-        	long long m1=a*c;
-        	long long m2=a*d;
-        	long long m3=b*c;
-        	long long m4=b*d;
-        	max=(m1+m2+ab(m1-m2))/2;
-        	max=(max+m3+ab(max-m3))/2;
-        	max=(max+m4+ab(max-m4))/2;
-        	printf("%lld",max);
-        }
+#include <stdio.h>
+
+int main() {
+	long long a,b,c,d;
+  	long long ans;
+  	scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
+  	ans = (a * c > a * d) ? a * c : a * d;
+  	ans = (ans > b * c) ? ans : b * c;
+  	ans = (ans > b * d) ? ans : b * d;
+  	printf("%lld", ans);
+  	return 0;
+}
