81058e6c8dd02153249f533746748d8ebcaab6c5
Quy Nguyen <youngboy5801@gmail.com>
2021-01-10 21:28:00 +0700

    Add solution for problem `A - Redundant Redundancy` Link: https://atcoder.jp/contests/arc110/tasks/arc110_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc110/submissions/19359478

--- atcoder/arc110/A.cpp
+++ atcoder/arc110/A.cpp
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    //freopen("task.in", "r", stdin);
-    //freopen("task.out", "w", stdout);
-    int64_t n;
-    cin >> n;
+void solve() {
+    int N;
+    cin >> N;
     int64_t ans = 1;
-    for (int64_t i = 2; i <= n; ++i) {
-        ans = ans * i / __gcd(ans,i);
+    for (int64_t i = 2; i <= N; ++i) {
+        ans = ans*i/__gcd(ans,i);
     }
-    cout << ans + 1;
+    cout << ans + 1 << "\n";
 }
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
