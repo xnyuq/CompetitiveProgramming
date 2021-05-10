50c774607adc815a8dab32a0c18e2c7fc31e9df1
Quy Nguyen <youngboy5801@gmail.com>
2021-05-10 18:26:00 +0700

    Add solution for problem `D - Same Differences` Link: https://codeforces.com/contest/1520/problem/D Tags: data structures, hashing, math, *1200 Ref: https://codeforces.com/contest/1520/submission/115857163

---
+++ codeforces/1520/D.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef QUYNX_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+#ifndef QUYNX_DEBUG 
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        int64_t ans = 0;
+        cin >> n;
+        unordered_map<int,int> mp;
+        int tmp;
+        for (int i = 0; i < n; ++i) {
+            cin >> tmp;
+            ans += mp[tmp-i];
+            ++mp[tmp-i];
+        }
+        cout << ans << "\n";
+    }
+}
