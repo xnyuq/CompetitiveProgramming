063b27bcbc79539dad2b34f488cc8747b32335d8
Quy Nguyen <youngboy5801@gmail.com>
2021-05-10 20:10:00 +0700

    Add solution for problem `A - Nastia and Nearly Good Numbers` Link: https://codeforces.com/contest/1521/problem/A Tags: constructive algorithms, math, number theory, *1000 Ref: https://codeforces.com/contest/1521/submission/115866299

---
+++ codeforces/1521/A.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef QUYNX_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+#ifndef QUYNX_DEBUG 
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        if (b == 1) cout << "NO\n";
+        else {
+            cout << "YES\n" << a << " " << 1LL*a*b << " " << 1LL*a*(b+1) << "\n";
+        }
+    }
+}
