19a9b85089afb21c14ef502e4155b8304c3175dd
Quy Nguyen <youngboy5801@gmail.com>
2021-05-26 17:06:00 +0700

    Add solution for problem `B1 - Palindrome Game (easy version)` Link: https://codeforces.com/contest/1527/problem/B1 Tags: constructive algorithms, games, *1200 Ref: https://codeforces.com/contest/1527/submission/117393812

---
+++ codeforces/1527/B1.cpp
@@ -0,0 +1,56 @@
+/**
+ * @author      : xnyuq
+ * @created     : 26/05/2021 16:58:51 +07
+ * @filename    : 1527b1
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef QUYNX_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+#ifndef QUYNX_DEBUG 
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int cnt = 0;
+        while (n--) {
+            char c;
+            cin >> c;
+            cnt += c == '0';
+        }
+        if (cnt == 1 || cnt % 2 == 0) cout << "BOB\n";
+        else cout << "ALICE\n";
+    }
+}
+
