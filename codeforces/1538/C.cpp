9787cb88f42356dda3672a4dadffdbea857703c1
Quy Nguyen <youngboy5801@gmail.com>
2021-07-02 19:08:00 +0700

    Add solution for problem `C - Number of Pairs` Link: https://codeforces.com/contest/1538/problem/C Tags: binary search, data structures, math, two pointers, *1300 Ref: https://codeforces.com/contest/1538/submission/121108988

---
+++ codeforces/1538/C.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef QUYNX_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+#ifndef QUYNX_DEBUG 
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n >> l >> r;
+        vector<int> arr(n);
+        for (auto& i: arr) cin >> i;
+        sort(arr.begin(), arr.end());
+        int64_t ans = 0;
+        for (int i = 0; i < n-1; ++i) {
+            ans += upper_bound(arr.begin() + i + 1, arr.end(), r - arr[i]) - lower_bound(arr.begin() + i + 1, arr.end(), l - arr[i]);
+            dbg(i, ans);
+        }
+        cout << ans << "\n";
+    }
+}
+
