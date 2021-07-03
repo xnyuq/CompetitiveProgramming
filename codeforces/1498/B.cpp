c552f450a53f9ff57a4fd75ecaf4c5b89821b486
Quy Nguyen <youngboy5801@gmail.com>
2021-07-03 13:37:00 +0700

    Add solution for problem `B - Box Fitting` Link: https://codeforces.com/contest/1498/problem/B Tags: binary search, bitmasks, data structures, greedy, *1300 Ref: https://codeforces.com/contest/1498/submission/121162130

---
+++ codeforces/1498/B.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef QUYNX_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+#ifndef QUYNX_DEBUG 
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, W;
+        cin >> n >> W;
+        vector<int> arr(n);
+        for (auto& i: arr) cin >> i;
+        sort(arr.rbegin(), arr.rend());
+        multiset<int> s;
+        s.insert(W);
+        for (int i = 0; i < n; ++i) {
+            auto it = s.lower_bound(arr[i]);
+            if (it == s.end()) s.insert(W-arr[i]);
+            else {
+                int left = *it - arr[i];
+                s.erase(it);
+                s.insert(left);
+            }
+        }
+        cout << (int)s.size() << "\n";
+    }
+}
+
