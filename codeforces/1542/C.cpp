940017b3f1609ea80fd34c128375da5c7782fafd
Quy Nguyen <youngboy5801@gmail.com>
2021-07-08 19:28:00 +0700

    Add solution for problem `C - Strange Function` Link: https://codeforces.com/contest/1542/problem/C Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/1542/submission/121723146

--- codeforces/1542/C.cpp
+++ codeforces/1542/C.cpp
@@ -1,26 +1,28 @@
+
 #include <bits/stdc++.h>
+
 
 const int64_t MOD = int(1e9)+7;
 
 int main() {
-	using namespace std;
-	ios_base::sync_with_stdio(false), cin.tie(nullptr);
 
-	int T = 1; cin >> T;
-	while (T--) {
-		int64_t N; cin >> N;
-
-		int64_t ans = N % MOD;
-		int64_t cur = 1;
-		for (int v = 1; true; v++) {
-			cur = std::lcm(cur, v);
-			if (cur > N) break;
-			ans += N / cur;
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int64_t N;
+        cin >> N;
+        int64_t ans = N % MOD;
+        int64_t cur = 1;
+        for (int i = 1;; ++i) {
+            cur = lcm(cur, i);
+            if (cur > N) break;
+            ans += N / cur;
             ans %= MOD;
-		}
-		cout << ans << '\n';
-	}
-
-
-	return 0;
+        }
+        cout << ans << "\n";
+    }
 }
+
