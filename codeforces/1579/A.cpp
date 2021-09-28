22db06eb6cb5e6d597f93a802da8ece320069c71
Quy Nguyen <youngboy5801@gmail.com>
2021-09-28 21:37:00 +0700

    Add solution for problem `A - Casimir's String Solitaire` Link: https://codeforces.com/contest/1579/problem/A Tags: math, strings, *800 Ref: https://codeforces.com/contest/1579/submission/130099945

---
+++ codeforces/1579/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef QUYNX_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+#ifndef QUYNX_DEBUG 
+    cin.tie(nullptr);
+#endif
+    int t; cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int a = 0, b = 0, c = 0;
+        for (auto& i: s) {
+            if (i == 'A') ++a;
+            else if (i == 'B') ++b;
+            else ++c;
+        }
+        if (a + c == b) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
+
